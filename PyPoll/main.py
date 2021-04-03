c021e8bfaf33493012729358a49044780f20c586
Alivia Varvel <atvarvel@gmail.com>
2021-04-03 14:56:16 -0400

    completed code without txt file output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -47,12 +47,27 @@ for vote in votes:
 
         fourth_candidate_votes += 1
 
+first_candidate_percent = (first_candidate_votes / total_votes) * 100
+second_candidate_percent = (second_candidate_votes / total_votes) * 100
+third_candidate_percent = (third_candidate_votes / total_votes) * 100
+fourth_candidate_percent = (fourth_candidate_votes / total_votes) * 100
+
+candidate_votes_list = [first_candidate_votes, second_candidate_votes, third_candidate_votes, fourth_candidate_votes]
+
+votes_max = max(candidate_votes_list)
+
+votes_max_location = candidate_votes_list.index(votes_max)
+
+votes_max_candidate = candidates[votes_max_location]
 
 print("Election Results")
 print("-----------------------")
 print(f"Total Votes: {total_votes}")
 print("-----------------------")
-print(f"{candidates[0]}: {first_candidate_votes}")
-print(f"{candidates[1]}: {second_candidate_votes}")
-print(f"{candidates[2]}: {third_candidate_votes}")
-print(f"{candidates[3]}: {fourth_candidate_votes}")
+print(f"{candidates[0]}: {round(first_candidate_percent, 3)}% ({first_candidate_votes})")
+print(f"{candidates[1]}: {round(second_candidate_percent, 3)}% ({second_candidate_votes})")
+print(f"{candidates[2]}: {round(third_candidate_percent, 3)}% ({third_candidate_votes})")
+print(f"{candidates[3]}: {round(fourth_candidate_percent, 3)}% ({fourth_candidate_votes})")
+print("-----------------------")
+print(f"Winner: {votes_max_candidate}")
+print("-----------------------")
